d25d1fbfd80da1b630b1e640e74fcb98447de133
Usama Mansoor <usamamansoor79@gmail.com>
2025-07-05 14:20:37 +0500

    Fixing the API urls

--- frontend/src/apiurls.js
+++ frontend/src/apiurls.js
@@ -1,12 +1,12 @@
 export const backendURL = import.meta.env.VITE_BACKEND_URL;
 export const apiUrls = {
-  loginAPI: `${backendURL}/auth/login`,
-  registerAPI: `${backendURL}/auth/register`,
-  paymentAPI: `${backendURL}/payment/create-checkout-session`,
-  fetchUserReservationsAPI: `${backendURL}/reservation/user`,
-  addReservationAPI: `${backendURL}/reservation/add`,
-  fetchUserOrdersAPI: `${backendURL}/order/getorders`,
-  placeOrderAPI: `${backendURL}/order/placeorder`,
-  fetchAllMenuItemsAPI: `${backendURL}/menu/getallitems`,
-  addFeedbackAPI: `${backendURL}/feedback/add`,
+  loginAPI: `${backendURL}/api/auth/login`,
+  registerAPI: `${backendURL}/api/auth/register`,
+  paymentAPI: `${backendURL}/api/payment/create-checkout-session`,
+  fetchUserReservationsAPI: `${backendURL}/api/reservation/user`,
+  addReservationAPI: `${backendURL}/api/reservation/add`,
+  fetchUserOrdersAPI: `${backendURL}/api/order/getorders`,
+  placeOrderAPI: `${backendURL}/api/order/placeorder`,
+  fetchAllMenuItemsAPI: `${backendURL}/api/menu/getallitems`,
+  addFeedbackAPI: `${backendURL}/api/feedback/add`,
 };
